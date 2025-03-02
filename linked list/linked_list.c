7296e92500de3aa8a2b8aeec2859b28ea4fe2e99
Asif <rahman8055lol@gmail.com>
2025-03-02 06:23:51 +0600

    Update linked_list.c

--- linked list/linked_list.c
+++ linked list/linked_list.c
@@ -10,7 +10,7 @@ struct node *head = (struct node*)malloc(sizeof(struct node));
 struct node *second = (struct node*)malloc(sizeof(struct node));
 
 head->data = 45;
-head->next = NULL;
+head->next = NULL;  
 
 printf("%d",head->data);
 
